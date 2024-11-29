f17c817e39aa19d07f602cda4dc9e14832755015
kgruiz <kgruiz@umich.edu>
2024-11-29 16:36:33 -0500

    Added Full Path Flattening

--- HTMLtoLatex.py
+++ HTMLtoLatex.py
@@ -13,15 +13,17 @@ def HTMLtoLatex(htmlFilePath: str) -> str:
     return pypandoc.convert_file(htmlFilePath, "tex")
 
 
-def HTMLDirtoLatex(htmlDir: str, latexDir: str) -> dict:
+def HTMLDirtoLatex(htmlDir: str, latexDir: str, fileNameFull: bool = False) -> dict:
 
     if htmlDir[-1] != "/":
 
         htmlDir += "/"
 
-    os.makedirs(latexDir, exist_ok=True)
+    if fileNameFull:
 
-    dirStructure = os.walk(top=htmlDir)
+        latexDir = f"Full Path {latexDir}"
+
+    os.makedirs(latexDir, exist_ok=True)
 
     dirStructure = os.walk(top=htmlDir)
 
@@ -30,6 +32,8 @@ def HTMLDirtoLatex(htmlDir: str, latexDir: str) -> dict:
     )
 
     dirStructure = os.walk(top=htmlDir)
+
+    i = 0
 
     with tqdm(total=totalFiles, desc="Converting HTML to LaTeX") as bar:
 
@@ -43,7 +47,7 @@ def HTMLDirtoLatex(htmlDir: str, latexDir: str) -> dict:
 
                     baseName = fileName.split(".html")[0]
 
-                    latexOutputDir = os.path.join(latexDir, root.split(htmlDir)[1])
+                    latexOutputDir = os.path.join(latexDir, root.replace(htmlDir, ""))
 
                     os.makedirs(latexOutputDir, exist_ok=True)
 
@@ -65,6 +69,12 @@ def HTMLDirtoLatex(htmlDir: str, latexDir: str) -> dict:
 
                     latexOutputPath = os.path.join(latexOutputDir, f"{baseName}.tex")
 
+                    if fileNameFull:
+
+                        fullFileName = f"{root.replace(htmlDir, "").replace("/", "_")}-{baseName}.tex"
+
+                        latexOutputPath = os.path.join(latexOutputDir, fullFileName)
+
                     with open(latexOutputPath, "w") as file:
 
                         file.write(latexFile)
@@ -72,9 +82,60 @@ def HTMLDirtoLatex(htmlDir: str, latexDir: str) -> dict:
                     bar.update()
 
 
+def FlattenDir(dirName: str) -> None:
+
+    if dirName.find(" ") == -1:
+
+        outDir = f"Flat-{dirName}"
+
+    else:
+
+        outDir = f"Flat {dirName}"
+
+    currentPath = os.path.abspath(dirName)
+
+    currentDir = currentPath[: currentPath.rfind("/")]
+
+    print(currentDir)
+
+    outDir = os.path.join(currentDir, outDir)
+
+    print(outDir)
+
+    os.makedirs(outDir, exist_ok=True)
+
+    directory = os.walk(dirName)
+
+    totalFiles = sum(
+        1 for _, _, files in directory for file in files if file.endswith(".html")
+    )
+
+    directory = os.walk(top=dirName)
+
+    with tqdm(total=totalFiles, desc=f"Flattening {dirName}") as bar:
+
+        for root, dirs, files in directory:
+
+            for fileName in files:
+
+                bar.set_postfix_str(f"{root.split("/")[-1]}/{fileName}", True)
+
+                outputName = os.path.join(outDir, os.path.basename(fileName))
+
+                with open(os.path.join(root, fileName), "r") as file:
+
+                    content = file.read()
+
+                with open(outputName, "w") as file:
+
+                    file.write(content)
+
+
 if __name__ == "__main__":
 
     htmlDir = "Raw HTML Files"
     latexDir = "Latex Pages"
 
-    HTMLDirtoLatex(htmlDir=htmlDir, latexDir=latexDir)
+    # HTMLDirtoLatex(htmlDir=htmlDir, latexDir=latexDir, fileNameFull=True)
+
+    FlattenDir(dirName=f"Full Path Latex Pages")
